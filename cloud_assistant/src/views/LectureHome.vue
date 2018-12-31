0a235c75c12d1448a0aaeac927b912beba4f9f6c
wujie <1348484709@qq.com>
2018-12-31 18:53:40 +0800

    update LectureHome Page

--- cloud_assistant/src/views/LectureHome.vue
+++ cloud_assistant/src/views/LectureHome.vue
@@ -1,10 +1,34 @@
 <template>
-    <div>LectureHome</div>
+    <div>
+        <van-cell title="按时间筛选" is-link @click="showpopup = true" />
+        <van-popup
+            v-model="showpopup"
+            position="bottom"
+        >
+        <van-picker
+            show-toolbar
+            title="按时间筛选"
+            :columns="columns"
+            @cancel="showpopup = false"
+            @confirm="showpopup = false"
+        />
+        </van-popup>
+    </div>
 </template>
 
 <style>
 </style>
 
 <script>
-export default {};
+export default {
+    data() {
+        return {
+            showpopup: false,
+            columns: ['-请选择-', '未来三天', '未来一周', '未来一个月']
+        };
+    },
+    methods: {
+
+    }
+};
 </script>
